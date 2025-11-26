24ce80f234940c6a7e7a364c563c6183178fedb1
Deyvid Blendal <154270623+deyvidblendal@users.noreply.github.com>
2025-11-26 11:20:55 +0000

    Atualizando o rodapé com os meus dados

--- front-end/src/ui/FooterBar.jsx
+++ front-end/src/ui/FooterBar.jsx
@@ -26,7 +26,7 @@ export default function FooterBar() {
         }}
       >
         Desenvolvido e mantido com <LocalCafeIcon fontSize="small" /> por <a 
-        href="mailto:professor@faustocintra.com.br">Prof. Fausto G. Cintra</a>
+        href="mailto:deyvid.rosa@fatec.sp.gov.br">Deyvid Blendal Conrado Rosa</a>
       </Typography>
     </Box>
   );
